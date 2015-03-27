fefbc5d3fb8de835958ea5d7886456716c901a68
Tomi Suomi <tomi.suomi@utu.fi>
2015-03-27 17:48:06 +0000

    Tukey's biweight fixed
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/PECA@101300 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/tukey.R
+++ R/tukey.R
@@ -1,4 +1,3 @@
-<<<<<<< .mine
 #---------------------
 # TUKEY BIWEIGHT
 #---------------------
@@ -7,12 +6,3 @@ tukey <- function(x, c=5, epsilon=1e-4, na.rm=TRUE) {
 	x <- x[!is.na(x)]
 	return <- tukey.biweight(x, c=c, epsilon=epsilon)
 }
-=======
-#---------------------
-# TUKEY BIWEIGHT
-#---------------------
-# Robust average of vector x with missing values ignored in the calculation.
-tukey <- function(x,c=5,epsilon=1e-4,na.rm=TRUE) {
-	x <- x[!is.na(x)]
-	return <- tukey.biweight(x,c=c,epsilon=epsilon)
-}>>>>>>> .r101298
