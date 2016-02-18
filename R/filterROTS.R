7e3f3a9ffd59778b2b0ba439ed7007d40765e976
Tomi Suomi <tomi.suomi@utu.fi>
2016-02-18 18:25:38 +0000

    Bug fix
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/PECA@113759 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/filterROTS.R
+++ R/filterROTS.R
@@ -8,17 +8,24 @@ ROTS.filtered <- function(data, groups, B, K, paired=FALSE, seed=NULL, a1=NULL, 
   # Store indexes and names
   filter <- sort(union(filter1,filter2))
   names <- rownames(data)[filter]
-  # Remove rows
-  data <- data[-filter,]
-  # Run ROTS
-  rots.out <- ROTS(data, groups, B, K, paired, seed, a1, a2)
-  # Fill back filtered rows with NA
-  for(i in 1:length(filter)) {
-    rots.out$d <- append(rots.out$d, NA, after=filter[i]-1)
-    names(rots.out$d)[filter[i]] <- names[i]
-    rots.out$p <- append(rots.out$p, NA, after=filter[i]-1)
-    rots.out$FDR <- append(rots.out$FDR, NA, after=filter[i]-1)
+  if (length(filter)>0) {
+   # Remove rows
+    data <- data[-filter,]
+    # Run ROTS
+    rots.out <- ROTS(data, groups, B, K, paired, seed, a1, a2)
+    # Fill back filtered rows with NA
+    for(i in 1:length(filter)) {
+      rots.out$d <- append(rots.out$d, NA, after=filter[i]-1)
+      names(rots.out$d)[filter[i]] <- names[i]
+      rots.out$p <- append(rots.out$p, NA, after=filter[i]-1)
+      rots.out$FDR <- append(rots.out$FDR, NA, after=filter[i]-1)
+    }
+    # Return results
+    return(rots.out)
+  } else {
+    # Run ROTS
+    rots.out <- ROTS(data, groups, B, K, paired, seed, a1, a2)
+    # Return results
+    return(rots.out)
   }
-  # Return results
-  return(rots.out)
 }
